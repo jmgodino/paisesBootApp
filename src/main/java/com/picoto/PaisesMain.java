47f2fd3dfa29ae98eca58ae3871a003ece3d3df2
GODINO MUNCHARAZ <l0098@senado.es>
2017-06-23 11:16:26 +0200

    Version inicial

---
+++ src/main/java/com/picoto/PaisesMain.java
@@ -0,0 +1,49 @@
+package com.picoto;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+
+@SpringBootApplication
+@EnableWebMvc
+@EnableMongoRepositories
+public class PaisesMain extends AbstractMongoConfiguration {
+	
+	@Value("${mongodb.url}")
+	private String mongoURL;
+	
+	@Value("${mongodb.collection}")
+	private String mongoCollection;
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaisesMain.class, args);
+	}
+
+	@Override
+	public String getDatabaseName() {
+		return mongoCollection;
+	}
+
+	@Override
+	@Bean
+	public Mongo mongo() throws Exception {
+		return new MongoClient(mongoURL);
+	}
+	
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer propertyPlaceholderConfigurer() {
+	    PropertySourcesPlaceholderConfigurer ppc = new PropertySourcesPlaceholderConfigurer();
+	    ppc.setLocation(new ClassPathResource("application.properties"));
+	    return ppc;
+	}
+	
+}
